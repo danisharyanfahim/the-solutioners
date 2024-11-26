751344243e93d64f94a688b01596686f93004990
Danish <danisharyanfahim@gmail.com>
2024-11-26 00:29:34 -0500

    deploy

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   // distDir: "build",
   reactStrictMode: false,
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
